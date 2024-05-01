9d6d5adade10cf0e0419ae92cfaa72c4b3e1908f
guilherme <guivascon05@gmail.com>
2024-05-01 17:20:40 -0300

    Adicionando Intruções

--- jogo_rpg/src/jogo/Codeland.java
+++ jogo_rpg/src/jogo/Codeland.java
@@ -1,7 +1,56 @@
 package jogo;
 
+import java.util.Scanner;
+
 public class Codeland {
+
+    public static void tituloJogo(String titulo) { // CLasse parar
+        System.out.println("#".repeat(titulo.length()));
+        System.out.println(titulo);
+        System.out.println("#".repeat(titulo.length()));
+    }
+
+    public static void introducao() { // classe para Definir o título e a introdução
+        String titulo = "\nSeja Bem-vindo à Codeland!!!\n";
+        tituloJogo(titulo);
+
+    }
+
+    public static void selecionarOpcao() {
+        Scanner scanner = new Scanner(System.in);
+        int escolha;
+
+        System.out.println("\nSelecione uma opção:"); // classe de Título para seleção de opções
+        System.out.println("1 - Instruções \n2 - Jogar \n3 - Créditos \n4 - Sair");
+        escolha = scanner.nextInt();
+        switch (escolha) {
+            case 1:
+                System.out.println("Clicou em Instruções!\n ");
+                break;
+            case 2:
+                System.out.println("Clicou em Jogar!\n ");
+                jogar();
+                break;
+            case 3:
+                System.out.println("Clicou em Créditos!\n ");
+                break;
+            case 4:
+                System.out.println("Clicou em Sair! Volte Sempre!\n ");
+                break;
+            default:
+                System.out.println("Opção inválida!\n");
+                break;
+        }
+        ; // Condição de saída do loop
+        scanner.close();
+    }
+
+    public static void instrucoes() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Teste");
+        introducao(); // Chamando introducao para exibir o título inicial
+        selecionarOpcao();
     }
 }
