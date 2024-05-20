4cc330cdb3b3d99e326481974f998ba14c2cb255
guilherme <guivascon05@gmail.com>
2024-05-20 02:38:43 -0300

    dialogo

--- jogo_rpg/src/jogo/Codeland.java
+++ jogo_rpg/src/jogo/Codeland.java
@@ -56,7 +56,7 @@ public class Codeland {
     }
 
     public static void jogar() {
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Capítulo 1: ");
         System.out.println("\r\n" + //
                 "   ___      ___                          _             \r\n" + //
@@ -73,7 +73,7 @@ public class Codeland {
             System.out.print(letra);
             try {
 
-                TimeUnit.MILLISECONDS.sleep(20);
+                TimeUnit.MILLISECONDS.sleep(0);// deixar 20
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -83,7 +83,7 @@ public class Codeland {
 
         try {
 
-            TimeUnit.SECONDS.sleep(10); // deixar como 20 no teste final
+            TimeUnit.SECONDS.sleep(0); // deixar como 15 no teste final
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -112,7 +112,7 @@ public class Codeland {
             System.out.print(caractere);
             try {
                 // Atraso nos caracteres
-                TimeUnit.MILLISECONDS.sleep(15); // deixar nos 15
+                TimeUnit.MILLISECONDS.sleep(0); // deixar nos 15
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -131,7 +131,7 @@ public class Codeland {
             System.out.print(letra);
             try {
 
-                TimeUnit.MILLISECONDS.sleep(15);
+                TimeUnit.MILLISECONDS.sleep(0);// mudar para 15
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -152,7 +152,7 @@ public class Codeland {
                 System.out.print(caractere);
                 try {
                     // Atraso do tempo para um efeito de digitação
-                    TimeUnit.MILLISECONDS.sleep(10);
+                    TimeUnit.MILLISECONDS.sleep(0);// mudar para 10
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -165,7 +165,7 @@ public class Codeland {
             System.out.print(letra);
             try {
 
-                TimeUnit.MILLISECONDS.sleep(15);
+                TimeUnit.MILLISECONDS.sleep(0);// mudar para 15
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -174,18 +174,63 @@ public class Codeland {
         String dialogo = "\nMentor Virtual: \"Bem-vindo, Code. Você foi escolhido para salvar CodeLand. Este mundo está em perigo devido ao NebulaShadow, que corrompeu nosso código, deixando nossa cidade toda em um caos total. Para restaurar a ordem, você deve enfrentar vários desafios envolvendo todo o seu conhecimento em programação, mais especificamente, você terá que usar o nosso queridinho 'JAVA'.\n"
                 +
                 "\nCode: 'Tinha que ser logo Java? A linguagem de programação que eu mais tenho dificuldade!'\n" +
-                "\nMentor Virtual: 'Não se preocupe! Eu estou aqui justamente para te auxiliar no começo para que você consiga derrotar o chefão.\nVamos depressa para a cidade, vou te mostrar o básico por agora!\n";
+                "\nMentor Virtual: 'Não se preocupe! Eu estou aqui justamente para te auxiliar no começo para que você consiga derrotar o chefão.\nVou te mostrar o básico por agora!";
 
         for (char letra : dialogo.toCharArray()) {
             System.out.print(letra);
             try {
 
-                TimeUnit.MILLISECONDS.sleep(15);
+                TimeUnit.MILLISECONDS.sleep(0); // deixar 15
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        int escolha = 0;
+        while (true) {
+            System.out.println("\nVocê está pronto?\n\n1 - Claro!\n2 - Estou com um pouco de Medo.\n");
+            escolha = scanner.nextInt();
+
+            if (escolha == 1) {
+                System.out.println("Que maravilha! Esse é o espírito de um verdadeiro Herói.\n Agora vamos lá!");
+                break;
+            } else if (escolha == 2) {
+                System.out.println(
+                        "Code, o escolhido! Não tema, pois será você quem trará a vitória para o nosso povo. Vamos lá para a cidade que eu lhe mostrarei o caminho.");
+                break;
+            } else {
+                System.out.println("Por favor, escolha uma opção válida (1 ou 2)!");
+            }
+        }
+
+        String primeirosPassos = "\nCode adentra naquela cidade futurista e claramente abandonada, um ambiente de desenvolvimento digital com códigos flutuando no ar e estruturas de dados animadas. Para uma pessoa comum, aquilo seria bizarro, mas para um programador, principalmente para 'Code', aquilo era Fantástico!\n"
+                + //
+                "O Mentor Virtual pede para Code parar um momento para que ele possa ensinar o básico dos fundamentos daquele mundo.\"\n";
+
+        for (char letra : primeirosPassos.toCharArray()) {
+            System.out.print(letra);
+            try {
+
+                TimeUnit.MILLISECONDS.sleep(0); // deixar 15
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
+        String primeiroConfronto = "\nCode adentra naquela cidade futurista e claramente abandonada, um ambiente de desenvolvimento digital com códigos flutuando no ar e estruturas de dados animadas. Para uma pessoa comum, aquilo seria bizarro, mas para um programador, principalmente para 'Code', aquilo era Fantástico!\n"
+                + //
+                "O Mentor Virtual pede para Code parar um momento para que ele possa ensinar o básico dos fundamentos daquele mundo.\"\n";
+
+        for (char letra : primeiroConfronto.toCharArray()) {
+            System.out.print(letra);
+            try {
+
+                TimeUnit.MILLISECONDS.sleep(0); // deixar 15
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        scanner.close();
     }
 
     public static void creditos() {
