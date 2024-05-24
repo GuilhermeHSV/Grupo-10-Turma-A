11b927c46115238badd794d4875699977e9c6437
giovanni <giovanni.c.gini@gmail.com>
2024-05-24 19:48:44 -0300

    teste

--- jogo_rpg/src/jogo/Codeland.java
+++ jogo_rpg/src/jogo/Codeland.java
@@ -61,7 +61,7 @@ public class Codeland {
     }
 
     public static void perguntaUm() {
-        String primeiraPergunta = "Você escolheu ficar e lutar contra o Monstro de metal!\nEle vem correndo em sua direção em alta velocidade e você fica paralisado de medo!\nMentor Virtual:'Code! Não se preocupe, ele não vai fazer nada com você... pelo menos por enquanto HAHAHA!'\nCode:'O QUE VOCÊ QUER DIZER COM ISSO?'";
+        String primeiraPergunta = "Você escolheu ficar e lutar contra o Monstro de metal!\nEle vem correndo em sua direção em alta velocidade e você fica paralisado de medo!\nMentor Virtual:'Code! Não se preocupe, ele não vai fazer nada com você... pelo menos por enquanto HAHAHA!'\nCode:O QUE VOCÊ QUER DIZER COM ISSO?";
 
         for (char letra : primeiraPergunta.toCharArray()) {
             System.out.print(letra);
